eeaa53505e2807e03520e3dc134eedbb5924d4c8
Homework2 <ivliev.k@yandex.ru>
2023-01-12 23:01:24 +0300

    Initial commit

--- src/Person.java
+++ src/Person.java
@@ -18,4 +18,8 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
+
+    public String toString() {
+        return "Имя " + this.name + " Возраст " + this.age;
+    }
 }
